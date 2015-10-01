52e0c6c952cce5dda9992c7e4d81120a8fa08c9b
Laith Azzam <Laith@l1.local>
2015-10-01 16:36:43 -0700

    v3

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -1,7 +1,29 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", function(event) { 
+	// var video = document.querySelector(".video-wrapper");
+	// var screenWrap = document.querySelector(".ntz-wrapper");
+	// var screenWrapHeight = screenWrap.offsetHeight;
+	// var videoHeight = video.offsetHeight;
+	// var videoTop = video.offsetTop;
+	// var marginTop = (( screenWrapHeight - videoHeight)/2) - videoTop/1.5;
+	// var marginBottom = (( screenWrapHeight - videoHeight)/2);
 
-setTimeout(function(){
-	var main = document.querySelector(".ntz-wrapper");
-	console.log( 'Javascript.' );
-	main.className = "ntz-wrapper active";
-}, 150);
+	// if(marginTop > 0){
+	// 	video.style.marginTop = marginTop + "px";
+	// 	video.style.marginBottom = marginBottom + "px";
+	// 	console.log(screenWrapHeight, videoHeight);
+	// }
+
+	var iframe = $('#video')[0];
+    var player = $f(iframe);
+
+    // When the player is ready, add listeners for pause, finish, and playProgress
+
+    // Call the API when a button is pressed
+    $('.play').bind('click', function(event) {
+        player.api('play');
+        return false;
+    });
+
+
+});
